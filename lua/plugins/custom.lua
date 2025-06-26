5e6e309e780f50efe13bc6462f9b2f1c10477209
someguy0555 <gjonas2005@gmail.com>
2025-06-26 19:46:00 +0300

    Idk

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,9 +1,3 @@
 return {
-    -- {
-    --     dir = "C:\\Users\\msjon\\Home\\Code\\Lua\\nvim-plugins\\snake",
-    --     -- config = function()
-    --     --     require("snake").setup()
-    --     -- end
-    -- },
     { "someguy0555/snake.nvim" },
 }
