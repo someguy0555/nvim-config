e383614ccceba143bb64a5a54da945ac61dcf502
someguy0555 <gjonas2005@gmail.com>
2025-07-03 21:45:33 +0300

    Added an autopair plugin, because I realised I wanted one :)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -103,4 +103,11 @@ return {
         },
         opts_extend = { "sources.default" }
     },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    },
 }
