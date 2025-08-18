e9c5584ade31346c277db71503602e68a026f274
someguy0555 <gjonas2005@gmail.com>
2025-08-18 23:37:21 +0300

    Created another branch for copilot and such, and removed it in this branch. Changed some blink.cmp settings.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,7 +8,6 @@ return {
         'saghen/blink.cmp',
         dependencies = {
             'rafamadriz/friendly-snippets',
-            'giuxtaposition/blink-cmp-copilot',
         },
         version = '1.*',
         opts = {
@@ -17,21 +16,19 @@ return {
                 ['<C-h>'] = { 'hide' },
                 ['<C-l>'] = { 'accept', 'fallback' },
                 ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-                -- ['<C-y>'] = { 'select_and_accept' },
                 ['<C-k>'] = { 'select_prev', 'fallback' },
                 ['<C-j>'] = { 'select_next', 'fallback' },
-                -- ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-                -- ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
                 ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
                 ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
                 ['<Tab>'] = { 'snippet_forward', 'fallback' },
                 ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-                ['<C-e>'] = { 'show_signature', 'hide_signature', 'fallback' },
+                ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
             },
             signature = {
                 enabled = true,
+                trigger = { enabled = false },
                 window = {
-                    show_documentation = false,
+                    -- show_documentation = false,
                 },
             },
             appearance = {
@@ -52,23 +49,22 @@ return {
                 },
                 menu = {
                     auto_show = true,
-                    draw = { columns = { { "kind_icon", "kind", gap = 1 }, { "label", "label_description", gap = 1 }, } },
+                    draw = { columns = { { "kind_icon", "kind", gap = 1 }, { "label", "label_description", gap = 1 }, {"source_name"} } },
+                    -- draw = { columns = { { "kind_icon", "kind", gap = 1 }, { "label", gap = 1 }, } },
                 },
+            },
+            fuzzy = {
+                implementation = "prefer_rust_with_warning",
+                -- sorts = {
+                --     'score',
+                --     'sort_text',
+                --     'label',
+                -- },
             },
             -- snippets = { preset = 'luasnip' },
             sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
-                providers = {
-                    copilot = {
-                        name = "copilot",
-                        module = "blink-cmp-copilot",
-                        score_offset = 100,
-                        async = true,
-                    },
-                },
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
             },
-            fuzzy = { implementation = "prefer_rust_with_warning" },
-            -- fuzzy = { implementation = "lua" },
         },
         opts_extend = { "sources.default" },
     },
